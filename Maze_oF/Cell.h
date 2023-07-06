d26409e9c2018e2ea0c029bb65f224ebe7c12548
cardorimezgo <ricardo@kilomx.com>
2023-07-05 22:56:02 -0600

    Update Cell.h

--- Maze_oF/Cell.h
+++ Maze_oF/Cell.h
@@ -1,12 +1,17 @@
 #pragma once
-
 #include <unordered_map>
 #include <vector>
+#include "Dir.h"
 
 struct Cell {
 
 	int row, col;
-	int north, south, east, west;
+
+	//linked cells
+	std::vector<Cell*>links;
+	
+	// Neighbors 
+	std::unordered_map<Direction, Cell*, EnumClassHash> neighbors;
 
 	Cell()
 };
