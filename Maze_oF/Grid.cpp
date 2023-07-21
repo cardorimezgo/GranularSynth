a14d94f042333197dd049b12f09503e40f64c616
cardorimezgo <cardori.mezgo@gmail.com>
2023-07-20 20:43:33 -0600

    Update Grid.cpp

--- Maze_oF/Grid.cpp
+++ Maze_oF/Grid.cpp
@@ -11,61 +11,60 @@ void Grid::DebugDisplay(){
 }
 
 void Grid::Display(){
- int margin = 20; // create constant value
- int cell_sz = 10; // create constant value
+
  for(int r = rows - 1; r >= 0; r--){
      for(int c = 0; c < cols; c++){
          Cell* cell = cells[r][c];
+
          float x1 = margin + (cell->row * cell_sz);
-         float y1 = margin + (cell->col + cell_sz);
+         float y1 = margin + (cell->col * cell_sz);
          float x2 = margin + ((cell->row + 1) * cell_sz);
          float y2 = margin + ((cell->col + 1) * cell_sz);
-         /////////////////////
-     }
- }
+
+         Cell* neighborN = cell->GetNeighbor(Direction::North);
+         if(neighborN == nullptr || !cell->Linked(neighborN)){
+             ofDrawLine(x1, y1, x2, y1);
+         }
+         Cell* neighborW = cell->GetNeighbor(Direction::West);
+         if(neighborW == nullptr || !cell->Linked(neighborW)){
+             ofDrawLine(x1, y1, x1, y2);
+         }
+         Cell* neighborE = cell->GetNeighbor((Direction::East));
+         if(neighborE == nullptr || !cell->Linked(neighborE)){
+             ofDrawLine(x2, y1, x2, y2);
+         }
+         Cell* neighborS = cell->GetNeighbor((Direction::South));
+         if(neighborS == nullptr || !cell->Linked(neighborS)){
+             ofDrawLine(x1, y2, x2, y2);
+         }
+      }
+   }
 }
 
-
-/*
-void Grid::Display(){
-    int gap= 20;
-    for(int r = rows - 1; r >= 0; r--){
-        //Print North wall
-        for(int c = 0; c < cols; c++){
-            Cell* cell = cells[r][c];
-            Cell* neighbor = cell->GetNeighbor(Direction::North);
-
-            //If neighbor does not exist or is bloqued of, display wall
-            if(neighbor == nullptr || !cell->Linked(neighbor)){
-                ofDrawLine(gap+15, gap, gap+30, gap); //oF line
-            }
-            else{
-            }
-        }
-        //Print west wall
-        for(int c = 0; c < cols; c++){
-            Cell* cell = cells[r][c];
-            Cell* neighbor = cell->GetNeighbor(Direction::West);
-            //If neighbor does not exist or is bloqued of, display wall
-            if(neighbor == nullptr || !cell->Linked(neighbor));
-        }
+bool Grid::Unlink(int r, int c, Direction dir){
+    Cell* cell = cells[r][c];
+    return cell->UnlinkCell(dir);
 }
+
+bool Grid::Link(int r, int c, Direction dir){
+    Cell* cell = cells[r][c];
+    return cell->LinkCell(dir);
 }
-*/
 
 bool Grid::Unmask(int r, int c){
    if(IsInvalid(r, c))
        return false;
 
     Cell* cell = cells[r][c];
-    if(!IsInvalid(r+1 , c))
-        cell->AddNeighbor(Direction::North, cells[r + 1][c]);
-    if(!IsInvalid(r-1 , c))
-        cell->AddNeighbor(Direction::South, cells[r - 1][c]);
-    if(!IsInvalid(r , c+1))
-        cell->AddNeighbor(Direction::East, cells[r][c + 1]);
-    if(!IsInvalid(r , c-1))
-        cell->AddNeighbor(Direction::West, cells[r][c - 1]);
+
+    if(!IsInvalid(r , c - 1))
+        cell->AddNeighbor(Direction::North, cells[r][c - 1]);
+    if(!IsInvalid(r , c + 1))
+        cell->AddNeighbor(Direction::South, cells[r][c + 1]);
+    if(!IsInvalid(r + 1 , c))
+        cell->AddNeighbor(Direction::East, cells[r + 1][c]);
+    if(!IsInvalid(r - 1 , c))
+        cell->AddNeighbor(Direction::West, cells[r - 1][c]);
    return true;
 }
 
