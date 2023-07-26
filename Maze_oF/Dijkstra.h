dd666a0260e6d2f9d6599bc585eaf7d06f442eaf
cardorimezgo <cardori.mezgo@gmail.com>
2023-07-25 23:50:27 -0600

    Update Dijkstra.h

--- Maze_oF/Dijkstra.h
+++ Maze_oF/Dijkstra.h
@@ -8,6 +8,7 @@ class DijkstraSolver{
     Grid& grid_;
     // -1 for unvisited, -2 for blocked, else distance from source
     std::vector<std::vector<int>> flood_fill;
+
     void SolveHelper(int r, int c, int distance){
         //return if invalid cell or already visited
         if(grid_.IsInvalid(r , c) || flood_fill[r][c] != -1)
@@ -18,6 +19,7 @@ class DijkstraSolver{
         auto neighbors = current_cell->GetNeighbors();
         for (auto neighbor : neighbors){
             if(current_cell->Linked(neighbor))
+                //adding +1 to unvisited neighbor
                 SolveHelper(neighbor->row, neighbor->col, distance + 1);
         }
     }
@@ -50,6 +52,7 @@ public:
         Cell* const nw_corner = grid_.GetCell(grid_.GetNumRows()-1, 0);
         Cell* const se_corner = grid_.GetCell(0, grid_.GetNumCols()-1);
         SolveHelper(nw_corner->row, nw_corner->col, 0);
+        //Print();
         //grid_.Display();
     }
 
