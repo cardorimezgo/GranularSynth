f5afbbd5eee2d91551852e86fb614ab3d66c484a
cardorimezgo <cardori.mezgo@gmail.com>
2023-09-12 23:34:11 -0600

    Update main.cpp

--- Maze_oF/main.cpp
+++ Maze_oF/main.cpp
@@ -3,11 +3,26 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
+    ofSetupOpenGL(800, 480, OF_WINDOW);			// <-------- setup the GL context
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
 
 }
+
+
+//TODO:
+// Introduce randomness into Prims and Kruskals algos. 
+//	-Leave option for adjusting weights in graphs depending... 
+//    on audio frequency analysis for example
+// implement Dijkstra algo
+// implement Kruskal algo
+// implement Prime's algo
+// Generate switch function for maze algos.
+// Reset Grid values when generating new mazes
+// find out how to integrate color gradient add-on
+// implementation of ofxColorMap to differentiate distances between cells
+// Implement Dither algorithm to maze 
+// Implement communication bia OSC with supercollider
+// Integrate touch interface RBPi
