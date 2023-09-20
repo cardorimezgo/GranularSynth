74a938097110f3688b54b7e61d002fb2a68f2dba
cardorimezgo <cardori.mezgo@gmail.com>
2023-09-19 22:12:36 -0600

    Update Weighted_Grid.h

--- Maze_oF/Weighted_Grid.h
+++ Maze_oF/Weighted_Grid.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <random>
 #include "Grid.h"
 
 class Weighted_Grid : public Grid{
@@ -6,20 +7,30 @@ class Weighted_Grid : public Grid{
 public:
     std::unordered_map<std::pair<Cell* , Cell*>, int , hash_Cell_Ptr> adjacencyList;
 
-    Weighted_Grid(int rows_, int cols_, int margin_, int cell_sz_) : Grid(rows_, cols_, margin_, cell_sz_){
-        //We need to connect each cell to its neighbors then set edge values
-        //Initialized all weights to zero  or data from audio processing     
+    Weighted_Grid(int rows_, int cols_, int margin_, int cell_sz_) : 
+    Grid(rows_, cols_, margin_, cell_sz_){        
+        init_Weights();
+        //set_Rnd_Edge(); ACTIVATE AFTER RUNNING TESTS
     }
 
-    bool SetWeight (int row1, int col1, int row2, int col2, int weight);
+    void init_Weights();
+
+    int get_Weight(Cell* cell1, Cell* cell2);
+
+    void SetWeight (int row1, int col1, int row2, int col2, int weight);
+    
+    
+    //set random weights for edges between cells
+    void set_Rnd_Edge();
+
+    
     
     /*
-    //set weight for edge between cells
-    bool random_Weight(Cell* cell);
-
     //set weight based on audio data
     bool audio_Weight(Cell* cell);
     */
+
+    void printAdjacencyList();
 };
 
 
