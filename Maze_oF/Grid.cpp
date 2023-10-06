c13eeef61ea9044a570c601bf2cface30de25f22
cardorimezgo <cardori.mezgo@gmail.com>
2023-10-05 23:09:08 -0600

    Update Grid.cpp

--- Maze_oF/Grid.cpp
+++ Maze_oF/Grid.cpp
@@ -9,55 +9,6 @@ void Grid::DebugDisplay(){
     }
 }
 
-//TODO:
-// find out how to integrate color gradient add-on
-// implementation of ofxColorMap to differentiate distances between cells
-
-void Grid::Display(){
-
-    //Create a 2D grid of rectangles
-    //std::vector<std::vector<ofRectangle>> rects;
-    DijkstraSolver dj(Grid); //RECALCULATE DIJKSTRA WHEN NEEDED, NOT ALL THE TIME
-
-    for(int r = rows - 1; r >= 0; r--){
-        for(int c = 0; c < cols; c++){
-            Cell* cell = cells[r][c];
-
-            float x1 = margin + (cell->row * cell_sz);
-            float y1 = margin + (cell->col * cell_sz);
-            float x2 = margin + ((cell->row + 1) * cell_sz);
-            float y2 = margin + ((cell->col + 1) * cell_sz);
-
-            // Intensity color of each cell
-            int color = dj.GetDistance(r , c);
-            ofSetColor(3*color, 3*color, 3*color, 200);
-
-            // Draw rectangles
-            //rects[r][c] = ofRectangle(x1, y1, cell_sz, cell_sz);
-            //ofDrawRectangle(rects[r][c]);
-            ofDrawRectangle(x1, y1, cell_sz, cell_sz);
-
-            //Draw Walls
-            Cell* neighborN = cell->GetNeighbor(Direction::North);
-            if(neighborN == nullptr || !cell->Linked(neighborN)){
-                ofDrawLine(x1, y1, x2, y1);
-            }
-            Cell* neighborW = cell->GetNeighbor(Direction::West);
-            if(neighborW == nullptr || !cell->Linked(neighborW)){
-                ofDrawLine(x1, y1, x1, y2);
-            }
-            Cell* neighborE = cell->GetNeighbor((Direction::East));
-            if(neighborE == nullptr || !cell->Linked(neighborE)){
-                ofDrawLine(x2, y1, x2, y2);
-            }
-            Cell* neighborS = cell->GetNeighbor((Direction::South));
-            if(neighborS == nullptr || !cell->Linked(neighborS)){
-                ofDrawLine(x1, y2, x2, y2);
-            }
-        }
-    }
-}
-
 bool Grid::Unlink(int r, int c, Direction dir){
     Cell* cell = cells[r][c];
     return cell->UnlinkCell(dir);
@@ -85,15 +36,14 @@ bool Grid::Unmask(int r, int c){
        return false;
 
     Cell* cell = cells[r][c];
-
-    if(!IsInvalid(r , c - 1))
-        cell->AddNeighbor(Direction::North, cells[r][c - 1]);
-    if(!IsInvalid(r , c + 1))
-        cell->AddNeighbor(Direction::South, cells[r][c + 1]);
-    if(!IsInvalid(r + 1 , c))
-        cell->AddNeighbor(Direction::East, cells[r + 1][c]);
-    if(!IsInvalid(r - 1 , c))
-        cell->AddNeighbor(Direction::West, cells[r - 1][c]);
+    if(!IsInvalid(r-1 , c))
+        cell->AddNeighbor(Direction::North, cells[r-1][c]);
+    if(!IsInvalid(r+1 , c))
+        cell->AddNeighbor(Direction::South, cells[r+1][c]);
+    if(!IsInvalid(r , c+1))
+        cell->AddNeighbor(Direction::East, cells[r][c+1]);
+    if(!IsInvalid(r , c-1))
+        cell->AddNeighbor(Direction::West, cells[r][c-1]);
    return true;
 }
 
