084cc395570de0c94aff6c8bee4f0ed5611cd33d
cardorimezgo <cardori.mezgo@gmail.com>
2023-10-24 22:16:05 -0600

    Update Cell_Renderer.cpp

--- Maze_oF/Cell_Renderer.cpp
+++ Maze_oF/Cell_Renderer.cpp
@@ -1,66 +1,50 @@
 #include "Cell_Renderer.h"
 
-void  Cell_Renderer::Draw_Walls(){
 
-    if(!needsRedraw)return;
+//Create flag for redrawing the maze (needsRedraw)
+void Cell_Renderer::Draw(int frame , const Grid& grid , const vector<pair<int , pair<int , int>>>& sorted){
+    
+    if(frame <= sorted.size()){
+        int color = sorted[frame].first;
+        int row = sorted[frame].second.first;
+        int col = sorted[frame].second.second;
 
-    for(int r = grid.GetNumRows() - 1; r >= 0; r--){
-        for(int c = 0; c < grid.GetNumCols(); c++){
-             Cell* cell = grid.GetCell(r , c);
+        float x1 = grid.GetMargin() + (col * grid.GetCell_Sz());
+        float y1 = grid.GetMargin() + (row * grid.GetCell_Sz());
+        float x2 = grid.GetMargin() + ((col + 1) * grid.GetCell_Sz());
+        float y2 = grid.GetMargin() + ((row + 1) * grid.GetCell_Sz());
 
-             // Calculating coordinates
-             float x1 = grid.GetMargin() + (cell->row * grid.GetCell_Sz());
-             float y1 = grid.GetMargin() + (cell->col * grid.GetCell_Sz());
-             float x2 = grid.GetMargin() + ((cell->row + 1) * grid.GetCell_Sz());
-             float y2 = grid.GetMargin() + ((cell->col + 1) * grid.GetCell_Sz());
-
-             //Draw Walls
-             Cell* neighborN = cell->GetNeighbor(Direction::North);
-             if(neighborN == nullptr || !cell->Linked(neighborN)){
-                 ofDrawLine(x1, y1, x2, y1);
-             }
-             Cell* neighborW = cell->GetNeighbor(Direction::West);
-             if(neighborW == nullptr || !cell->Linked(neighborW)){
-                 ofDrawLine(x1, y1, x1, y2);
-             }
-             Cell* neighborE = cell->GetNeighbor((Direction::East));
-             if(neighborE == nullptr || !cell->Linked(neighborE)){
-                 ofDrawLine(x2, y1, x2, y2);
-             }
-             Cell* neighborS = cell->GetNeighbor((Direction::South));
-             if(neighborS == nullptr || !cell->Linked(neighborS)){
-                 ofDrawLine(x1, y2, x2, y2);
-             }
-         }
-     }
-     needsRedraw = false;
-}
-
-void Cell_Renderer::Draw_Rects(){
-    vector<pair<int , pair<int , int>>> sorted = dj.Sort_Distance();
-    for(const auto& entry : sorted){
-
-        int color = entry.first;
-        int row = entry.second.first;
-        int col = entry.second.second;
-
-        float x1 = grid.GetMargin() + (row * grid.GetCell_Sz());
-        float y1 = grid.GetMargin() + (col * grid.GetCell_Sz());
-
-        // Intensity color of each cell
-        ofSetColor(3*color, 3*color, 3*color, 200);
-        //std::cout << std::setw(2) << color << " ";
+        // Intensity color of each cell //Filling Cells
+        ofSetColor(0.5*color, 0.5*color, 0.5*color, 220);
         ofDrawRectangle(x1 , y1, grid.GetCell_Sz(), grid.GetCell_Sz());
+        
+        //Drawing Walls
+        Cell* cell = grid.GetCell(row , col);
 
+        Cell* neighborN = cell->GetNeighbor(Direction::North);
+        if(neighborN == nullptr || !cell->Linked(neighborN)){
+            ofSetColor(255, 255, 255, 100);
+            ofDrawLine(x1, y1, x2, y1);
+        }
+        Cell* neighborW = cell->GetNeighbor(Direction::West);
+        if(neighborW == nullptr || !cell->Linked(neighborW)){
+            ofSetColor(255, 255, 255, 100);
+            ofDrawLine(x1, y1, x1, y2);
+        }
+        Cell* neighborE = cell->GetNeighbor((Direction::East));
+        if(neighborE == nullptr || !cell->Linked(neighborE)){
+            ofSetColor(255, 255, 255, 100);
+            ofDrawLine(x2, y1, x2, y2);
+        }
+        Cell* neighborS = cell->GetNeighbor((Direction::South));
+        if(neighborS == nullptr || !cell->Linked(neighborS)){  
+            ofSetColor(255, 255, 255, 100);
+            ofDrawLine(x1, y2, x2, y2);
+        }        
+    }else{
+        ofLog() << "Frame index out of bounds" << frame;
     }
 }
 
-void Cell_Renderer::Anima_Maze(int cnt){
-    ofSetColor(0,0,0,200);
-    ofDrawCircle(20, cnt, 50);
-}
 
 
-//TODO:
-// find out how to integrate color gradient add-on
-// implementation of ofxColorMap to differentiate distances between cells
