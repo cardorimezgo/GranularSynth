e88932ada381d7b48e439e0d7232b9ab3630ccb3
cardorimezgo <cardori.mezgo@gmail.com>
2023-11-07 18:42:30 -0600

    Add files via upload

--- Maze_oF/Sidewinder.h
+++ Maze_oF/Sidewinder.h
@@ -9,32 +9,30 @@ public:
 	{}
 
 	void Generate(int i_r, int i_c) override {
+		std::uniform_int_distribution<int> e_or_n(1, 2);
 
-			for (int r = maze_.GetNumRows()- 1; r >= 0; r--) {
-				int group_run = 0;
-				for (int c = 0; c < maze_.GetNumCols(); c++) {
-					std::uniform_int_distribution<int> e_or_n(0, 1);
-					bool io = e_or_n(rng_) != 0;
+		for (int r = 0; r < maze_.GetNumRows() - 1; r++) {
+		//for (int r = maze_.GetNumRows() - 1; r > 0; r--) {
+		int group_run = 0;
+			for (int c = 0; c < maze_.GetNumCols(); c++) {		
+				int rand_int = e_or_n(rng_);
+				
+				if (rand_int == 2) { //If not at east border Open east wall
+					if (!maze_.Link(r, c, Direction::East)){ // if we are at the last col, carve north
+						rand_int = 1;
+					}
+				}
 
-					if(io){
-						if (!maze_.Link(r, c, Direction::East))
-							io = 0;
-					}
-					else{
-						// Select random cell from the group of cells visited
-						if (!group.empty()) {
-							std::uniform_int_distribution<> group_c(0, group.size() - 1);
-							int ran_index = group_c(rng_);
-							maze_.Link(r, ran_index, Direction::North);
-						}
-						else {
-							maze_.Link(r, c, Direction::North);
-						}
-						group.clear();
-					}
-					num_cells++;
+				if (rand_int == 1) { //If at the last column Open North wall for one of the cells in the group
+					std::uniform_int_distribution<> group_c(group_run, c);
+					int ran_index = group_c(rng_);
+					maze_.Link(r, ran_index, Direction::North);
+					group_run = c + 1;
 				}
 			}
+		}
+		for (int c = 0; c < maze_.GetNumCols(); c++) {
+			maze_.Link(maze_.GetNumRows() - 1, c, Direction::East);
 		}
 	}
 };
