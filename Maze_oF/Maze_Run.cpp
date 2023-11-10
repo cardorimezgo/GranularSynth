c589108da2bca909be18959d5ea12cc6adfcc262
cardorimezgo <cardori.mezgo@gmail.com>
2023-11-09 22:43:55 -0600

    Add files via upload

---
+++ Maze_oF/Maze_Run.cpp
@@ -0,0 +1,13 @@
+#include "Maze_Algos.h"
+
+void Setup_Maze(Maze_Algos type) {
+
+
+
+	switch (type) {
+	case Maze_Algos::Binary_Tree:
+		bt.Generate(0, 0);
+		//dfs.Solve(GRID_DIM_Y -1,0);
+		//dfs.printGridFlat();
+	}
+}
