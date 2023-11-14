d877178f07ea15338bc122b506acb52925db48be
cardorimezgo <cardori.mezgo@gmail.com>
2023-11-13 23:18:17 -0600

    Add files via upload

---
+++ Maze_oF/Maze_Run.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Grid"
+#include "Maze_Algos.h"
+#include "Maze_Solver.h"
+#include "Binary_Tree.h"
+
+class Maze_Run {
+
+
+public:
+
+	void Setup_Maze(Maze_Algos type);
+
+};
