b14306d00370de206e5b6f56822990d76f7caefa
cardorimezgo <cardori.mezgo@gmail.com>
2023-11-17 18:31:10 -0600

    Update ofApp.h

--- Maze_oF/ofApp.h
+++ Maze_oF/ofApp.h
@@ -8,8 +8,13 @@
 #include "Cell_Renderer.h"
 #include "Prim's.h"
 #include "Binary_Tree.h"
+#include "Sidewinder.h"
+#include "Maze_Solver.h"
+#include "Maze_Algos.h"
+#include "Maze_Run.h"
+#include "Maze_Sz.h"
 
-//Grid variables 
+//Grid variables   /// the number of cols and rows arent constant, will change according to user's needs
 constexpr auto GRID_DIM_X = 100; ///cols//100///50///25///10
 constexpr auto GRID_DIM_Y = 60; //rows////60///30///15////6
 constexpr auto MARG = 0;
@@ -19,35 +24,51 @@ constexpr auto CELL_SZ = 8; //////////////8///16///32///80
 const int FPS = 60;
 const int MILLISECS_PER_FRAME = 1000 / FPS;
 
+enum AppState {
+	WAITING_FOR_INPUT,
+	MAZE_GENERATION,
+	MAZE_GENERATED
+};
 
+class ofApp : public ofBaseApp {
 
-class ofApp : public ofBaseApp{
+public:
+	void setup();
+	void update();
+	void draw();
 
-	public:
-		void setup();
-		void update();
-		void draw();
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-        void gotMessage(ofMessage msg);
-		
-        int total_cells;
+	int total_cells = 0;
 
-        ofFbo Draw_Buffer;
-        Grid maze = Grid(GRID_DIM_Y, GRID_DIM_X, MARG, CELL_SZ);
-		Weighted_Grid weight_maze = Weighted_Grid(GRID_DIM_Y, GRID_DIM_X, MARG, CELL_SZ);
-		Depth_First_Search dfs = Depth_First_Search(maze);
-        DijkstraSolver dj = DijkstraSolver(weight_maze);
-		BinaryTreeGenerator bt = BinaryTreeGenerator(maze);
-		Prim prim = Prim(weight_maze);
-        Cell_Renderer render = Cell_Renderer();
+	///KEYBOARD INTERACTION
+	bool set_size = false;
+	bool set_maze = false;
+	/////////////////////
+
+	ofFbo Draw_Buffer;
+	Grid maze = Grid(GRID_DIM_Y, GRID_DIM_X, MARG, CELL_SZ);
+	Weighted_Grid weight_maze = Weighted_Grid(GRID_DIM_Y, GRID_DIM_X, MARG, CELL_SZ);
+	Depth_First_Search dfs = Depth_First_Search(maze);
+	Dijkstra dj = Dijkstra(weight_maze);
+	BinaryTreeGenerator bt = BinaryTreeGenerator(maze);
+	Sidewinder sw = Sidewinder(maze);
+	Prim prim = Prim(weight_maze);
+	Maze_Algos maze_algo;
+	Maze_Run run;
+	Maze_Sz sz;
+	Cell_Renderer c_render = Cell_Renderer(maze , dfs);
+
+private:
+	AppState currentState;
 };
